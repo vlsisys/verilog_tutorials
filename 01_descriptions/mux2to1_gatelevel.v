2464f1be4b22ef69b60740053794bf96f6aecc58
vlsisys <woongchoi@sm.ac.kr>
2024-04-30 18:11:53 +0900

    comment

--- 01_descriptions/mux2to1_gatelevel.v
+++ 01_descriptions/mux2to1_gatelevel.v
@@ -1,5 +1,4 @@
-module	mux2to1_gatelevel
-(
+module	mux2to1_gatelevel(
 	output		o_out,
 	input		i_sel,
 	input		i_in0,
@@ -10,8 +9,8 @@ module	mux2to1_gatelevel
 	wire		out_1;
 
 	not	u_not0	(sel_n , i_sel         ) ;
-	and	u_and0	(out_0 , i_sel         ) ;
-	and	u_and1	(out_1 , sel_n         ) ;
+	and	u_and0	(out_0 , i_in0 , sel_n ) ;
+	and	u_and1	(out_1 , i_in1 , i_sel ) ;
 	or	u_or0	(o_out , out_0 , out_1 ) ;
 
 endmodule
--- 01_descriptions/mux2to1_gatelevel_tb.v
+++ 01_descriptions/mux2to1_gatelevel_tb.v
@@ -1,10 +1,14 @@
+`include	"mux2to1_gatelevel.v"
+
 module	mux2to1_gatelevel_tb;
 
-	wire	o_out;
+	reg	[8*32-1:0]	vcd_file;
 
-	reg		i_sel;
-	reg		i_in0;
-	reg		i_in1;
+	wire			o_out;
+
+	reg				i_sel;
+	reg				i_in0;
+	reg				i_in1;
 
 	mux2to1_gatelevel
 	dut(
@@ -17,12 +21,29 @@ module	mux2to1_gatelevel_tb;
 	integer	i;
 	initial begin
 		{i_sel, i_in0, i_in1} = 3'b000;
-		$monitor ("T=%0t:	sel=%0b	in0/1=%0b/%0b",	$time,	i_sel, i_in0, i_in1);
+		#(10);
+		$monitor("T=%0t		sel=%0b	in0/1=%0b/%0b out=%0b",	$time,	i_sel, i_in0, i_in1, o_out);
 		for	(i=0; i<10; i++) begin
-			#1
-			i_sel	= $random;
-			i_in0	= $random;
-			i_in1	= $random;
+			#(1);
+				i_sel	= $urandom_range(0,1);
+				i_in0	= $urandom_range(0,1);
+				i_in1	= $urandom_range(0,1);
+			#(9);
 		end
 	end
+	initial begin
+		if ($value$plusargs("vcd_file=%s", vcd_file)) begin
+			$dumpfile(vcd_file);
+			$dumpvars;
+		end
+	end
+
+	task dispAsFixedPoint;
+		input reg	[127:0]					sDescription;
+		input		[`DEF_BIT_FULL-1:0]	bIn;
+		begin
+			$display("[%0s] Binary: %b > Fixed Point Number: %f", sDescription, bIn, bIn*(2.0**-(`DEF_BIT_FRAC)));
+		end
+	endtask
+
 endmodule
